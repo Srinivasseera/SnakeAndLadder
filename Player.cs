2e669c97ca57ba9140248ff618689aca03963c5b
Srinivasseera <srinivasseera7@gmail.com>
2024-05-27 10:06:48 +0530

    adding the player

---
+++ Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder.Game
+{
+    public class Player
+    {
+        public int Position { get; set; }
+        public int DiceRollCount { get; set; }
+        public Player() 
+        {
+            Position = 0;
+            DiceRollCount = 0;
+        }
+        public void UpdatePosition(int move)
+        {
+            Position += move;
+            if(Position < 0)
+            {
+                Position = 0;
+            }
+            DiceRollCount++;
+        }
+        public void SetPosition(int position) 
+        {
+            Position = position;
+        }
+    }
+}
